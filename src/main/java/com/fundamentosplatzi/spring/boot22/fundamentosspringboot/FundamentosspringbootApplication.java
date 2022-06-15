daab85087766bfea1b96a74af50e35c4c24366d2
DESKTOP-LK5G5KM\Sanchez <Miguel Sanchez>
2022-06-15 12:36:49 -0500

    firts commit marsan

---
+++ src/main/java/com/fundamentosplatzi/spring/boot22/fundamentosspringboot/FundamentosspringbootApplication.java
@@ -0,0 +1,14 @@
+package com.fundamentosplatzi.spring.boot22.fundamentosspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosspringbootApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FundamentosspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosplatzi/spring/boot22/fundamentosspringboot/FundamentosspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.spring.boot22.fundamentosspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
